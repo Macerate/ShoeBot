b3e3584e0062a7ded26f1785df9e134cb822e48b
EC2 Default User <ec2-user@ip-172-31-28-108.us-east-2.compute.internal>
2019-03-12 18:53:59 +0000

    Yuh

---
+++ bot.js
@@ -0,0 +1,68 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+const colors = require("colors");
+
+const config = require("./config.json");
+const client = new Discord.Client();
+client.config = config;
+
+//Run once loaded
+
+client.on("ready", () => {
+	console.log("Successfully loaded!".rainbow.bold.underline + `\nPrefix: \"${config.prefix}\"`.bold);
+});
+
+//Load Events
+
+fs.readdir("./events/", (err, files) => {
+
+	console.log("Attempting to load events...".bold.yellow);
+
+	if (err) return console.error(err.red.bold);
+
+	files.forEach(file => {
+		if (!file.endsWith(".js")) return;
+
+		const event = require(`./events/${file}`);
+
+		let eventName = file.split(".")[0];
+
+		console.log(`Attempting to load event ${eventName}`.yellow);
+
+		client.on(eventName, event.bind(null, client));
+
+		delete require.cache[require.resolve(`./events/${file}`)];
+
+		console.log(`Event \"${eventName}\" loaded!`.green);
+	});
+	console.log(`All events loaded successfully!`.green.bold);
+});
+
+//Load commands
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+
+	console.log("Attempting to load commands...".bold.yellow);
+
+	if (err) return console.error(err);
+
+	files.forEach(file => {
+		if (!file.endsWith(".js")) return;
+
+		let props = require(`./commands/${file}`);
+
+		let commandName = file.split(".")[0];
+
+		console.log(`Attempting to load command ${commandName}`.yellow);
+
+		client.commands.set(commandName, props);
+
+		console.log(`Event \"${commandName}\" loaded!`.green);
+	});
+	console.log(`All commands loaded successfully!`.green.bold);
+});
+
+client.login(config.token);
---
+++ commands/reload.js
@@ -0,0 +1,19 @@
+exports.run = (client, message, args) => {
+    if (message.member.id == client.config.ownerID) {
+        if (args.length == 0) return message.reply("Must provide a command name to reload.");
+
+        const commandName = args[0];
+
+        if (!client.commands.has(commandName)) return message.reply("That command does not exist");
+
+        delete require.cache[require.resolve(`./${commandName}.js`)];
+
+        client.commands.delete(commandName);
+        const props = require(`./${commandName}.js`);
+        client.commands.set(commandName, props);
+        message.reply(`The command \"${commandName}\" has been reloaded`);
+    }
+    else {
+        message.reply("You don't have permission to do that!");
+    }
+};
